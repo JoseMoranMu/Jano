a7578873dd48c3d4e90a6fb61af745e21f9e6e25
OlgaColl <ocoll94@gmail.com>
2017-07-05 15:27:23 +0200

    Commit db a medias

--- app/src/main/java/android/app/jano/model/DBConnect.java
+++ app/src/main/java/android/app/jano/model/DBConnect.java
@@ -1,5 +1,6 @@
 package android.app.jano.model;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,13 +10,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBConnect extends SQLiteOpenHelper {
 
+    String createSQL = "CREATE TABLE prueba (active BOOL)";
+    String insertSQL = "INSERT INTO settings (active) VALUES (1)";
+
+    public DBConnect(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(this.createSQL);
+        db.execSQL(this.insertSQL);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS settings");
+        db.execSQL(this.createSQL);
     }
 }
