dfc19ab70cdbde916c6370727e6fb217e40f7d9f
JoseMoranMu <josemoranmu@gmail.com>
2017-07-05 16:03:21 +0200

    Create Classes

---
+++ app/src/main/java/android/app/jano/model/Earning.java
@@ -0,0 +1,56 @@
+package android.app.jano.model;
+
+import java.util.Date;
+
+/**
+ * Created by Jose on 05/07/2017.
+ */
+
+public class Earning {
+    double value;
+    String description;
+    Date date;
+    Category category;
+
+    public Earning() {
+    }
+
+    public Earning(double value, String description, Date date, Category category) {
+        this.value = value;
+        this.description = description;
+        this.date = date;
+        this.category = category;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
