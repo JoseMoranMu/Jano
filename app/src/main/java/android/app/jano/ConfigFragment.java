3945957ee9eeabc7118d11fed6b930d553a91de8
JoseMoranMu <josemoranmu@gmail.com>
2017-07-05 18:29:53 +0200

    Calendary dialog

--- app/src/main/java/android/app/jano/ConfigFragment.java
+++ app/src/main/java/android/app/jano/ConfigFragment.java
@@ -1,11 +1,14 @@
 package android.app.jano;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by olgacoll on 5/7/17.
@@ -23,6 +26,29 @@ public class ConfigFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_config, container, false);
+        initComponents(view);
         return view;
+    }
+
+    private void initComponents(View view) {
+        Button b = (Button) view.findViewById(R.id.buttonAlert);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog d = createDialog();
+                d.show();
+            }
+        });
+    }
+    public Dialog createDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.test_layout, null));
+        // Add action buttons
+        return builder.create();
     }
 }
