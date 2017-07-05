ca05ba0b16da748a4d663e1009b82fe022563e26
JoseMoranMu <josemoranmu@gmail.com>
2017-07-05 16:39:48 +0200

    DB espense and earning

--- app/src/main/java/android/app/jano/model/DBConnect.java
+++ app/src/main/java/android/app/jano/model/DBConnect.java
@@ -11,8 +11,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBConnect extends SQLiteOpenHelper {
 
-    String createEarningSQL = "CREATE TABLE earning (id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, description TEXT, date TEXT, category_id INTEGER)";
-    String createExpenseSQL = "CREATE TABLE expense (id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, description TEXT, date TEXT, category_id INTEGER)";
+    String createEarningSQL = "CREATE TABLE earning (id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, description TEXT, date TEXT, category_id INTEGER, FOREIGN KEY(category_id) REFERENCES category(id))";
+    String createExpenseSQL = "CREATE TABLE expense (id INTEGER PRIMARY KEY AUTOINCREMENT, value REAL, description TEXT, date TEXT, category_id INTEGER, FOREIGN KEY(category_id) REFERENCES category(id))";
     String createCategorySQL = "CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, image TEXT)";
 
     public DBConnect(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -21,26 +21,30 @@ public class DBConnect extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(this.createCategorySQL);
         db.execSQL(this.createEarningSQL);
         db.execSQL(this.createExpenseSQL);
-        db.execSQL(this.createCategorySQL);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS category");
+        db.execSQL(this.createCategorySQL);
         db.execSQL("DROP TABLE IF EXISTS earning");
         db.execSQL(this.createEarningSQL);
         db.execSQL("DROP TABLE IF EXISTS expense");
         db.execSQL(this.createExpenseSQL);
-        db.execSQL("DROP TABLE IF EXISTS category");
-        db.execSQL(this.createCategorySQL);
     }
+
+    //CATEGORY
 
     public void addCategory(Category category){
         SQLiteDatabase sqlConfig;
         sqlConfig = this.getWritableDatabase();
         if (sqlConfig != null) {
-            sqlConfig.execSQL("INSERT INTO category (name, image) VALUES ('" + category.getName() + "', '" + category.getImage() + "')");
+            sqlConfig.execSQL("INSERT INTO category (name, image) VALUES ('" + category.getName() + "', '"
+                                                                            + category.getImage() + "')");
         }
         sqlConfig.close();
     }
@@ -49,7 +53,9 @@ public class DBConnect extends SQLiteOpenHelper {
         SQLiteDatabase sqlConfig;
         sqlConfig = this.getWritableDatabase();
         if (sqlConfig != null) {
-            sqlConfig.execSQL("UPDATE category SET name = '" + category.getName() + "', image = '" + category.getImage() + "' WHERE id = " + category.getId() + "')");
+            sqlConfig.execSQL("UPDATE category SET name = '" + category.getName() +
+                                                "', image = '" + category.getImage() +
+                                                "' WHERE id = " + category.getId() + "')");
         }
         sqlConfig.close();
     }
@@ -63,6 +69,78 @@ public class DBConnect extends SQLiteOpenHelper {
         sqlConfig.close();
     }
 
+    //EARNING
+
+    public void addEarning(Earning earning){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("INSERT INTO earning (value, description, date, category_id) VALUES ('" + earning.getValue() + "', '"
+                                                                                                    + earning.getDescription() + "', '"
+                                                                                                    + earning.getDate() + "', '"
+                                                                                                    + earning.getCategory().getId() + "'  )");
+        }
+        sqlConfig.close();
+    }
+
+    public void modifyEarning(Earning earning){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("UPDATE earning SET value = '" + earning.getValue()  +
+                                                "', desciption = '" + earning.getDescription()  +
+                                                "', date = '" + earning.getDate() +
+                                                "', category_id = '" + earning.getCategory().getId() +
+                                                "' WHERE id = " + earning.getId() + "')");
+        }
+        sqlConfig.close();
+    }
+
+    public void removeEarning(Earning earning){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("DELETE FROM earning WHERE id = " + earning.getId() + "')");
+        }
+        sqlConfig.close();
+    }
+
+    //EXPENSE
+    
+    public void addExpense(Expense expense){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("INSERT INTO expense (value, description, date, category_id) VALUES ('" + expense.getValue() + "', '"
+                    + expense.getDescription() + "', '"
+                    + expense.getDate() + "', '"
+                    + expense.getCategory().getId() + "'  )");
+        }
+        sqlConfig.close();
+    }
+
+    public void modifyExpense(Expense expense){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("UPDATE expense SET value = '" + expense.getValue()  +
+                    "', desciption = '" + expense.getDescription()  +
+                    "', date = '" + expense.getDate() +
+                    "', category_id = '" + expense.getCategory().getId() +
+                    "' WHERE id = " + expense.getId() + "')");
+        }
+        sqlConfig.close();
+    }
+
+    public void removeExpense(Expense expense){
+        SQLiteDatabase sqlConfig;
+        sqlConfig = this.getWritableDatabase();
+        if (sqlConfig != null) {
+            sqlConfig.execSQL("DELETE FROM expense WHERE id = " + expense.getId() + "')");
+        }
+        sqlConfig.close();
+    }
+    /**
     public void showData(){
         String name;
         Cursor c;
@@ -77,4 +155,5 @@ public class DBConnect extends SQLiteOpenHelper {
             c.close();
         }
     }
+     **/
 }
